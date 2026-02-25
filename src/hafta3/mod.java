a00af34f3129c838ecbd24d7487beb0726d86f26
edafulin <edafuliin@gmail.com>
2026-02-25 11:08:41 +0300

    ekleme

---
+++ src/hafta3/mod.java
@@ -0,0 +1,31 @@
+package hafta3;
+import java.util.Scanner;
+
+public class mod {
+    public static void main(String[] args) {
+
+        Scanner giris = new Scanner(System.in);
+
+        int sayi , kalan ;
+        
+        System.out.println("sayi giriniz:");
+        sayi = giris.nextInt();
+
+        kalan = sayi % 2 ;
+        
+        if (kalan==0){
+            System.out.println("sayınız çifftir.");
+
+        }else{
+            System.out.println("sayınız tektir.");
+
+        }
+
+        giris.close();
+
+    }
+
+    
+
+    
+}
